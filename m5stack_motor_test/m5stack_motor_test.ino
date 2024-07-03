7566dc93127a50c4fb4e6c043997062d2417f69a
taisyu shibata <t_shiba117@yahoo.co.jp>
2024-07-04 03:49:22 +0900

    Edit

--- m5stack_motor_test/m5stack_motor_test.ino
+++ m5stack_motor_test/m5stack_motor_test.ino
@@ -1,38 +1,39 @@
 #include <HardwareSerial.h>
 
-HardwareSerial mySerial(2); // RX=16, TX=17を利用
+HardwareSerial mySerial(2); // 使用するHardwareSerialポートを指定
 
 void setup() {
   Serial.begin(115200); // デバッグ用シリアルポートを開始
   mySerial.begin(115200, SERIAL_8N1, 16, 17); // モータードライバとのUART通信を開始
+
+  // 5 RPMの速度を設定するコマンドを送信
+  int rpm = 5; // RPM値
+  sendVelocityCommand(0x70B1, rpm);
 }
 
 void loop() {
-  int targetSpeedRpm = 100; // 速度指令値（RPM）
-  setTargetVelocity(targetSpeedRpm); // 速度を設定する関数を呼び出し
-
-  delay(2000); // 次のコマンド送信までの間隔
+  // モータードライバからの応答を受信して表示
+  if (mySerial.available()) {
+    Serial.print("Received: ");
+    while (mySerial.available()) {
+      Serial.print(mySerial.read(), HEX);
+      Serial.print(" ");
+    }
+    Serial.println();
+  }
+  delay(1000); // 次の読み取りまで1秒待機
 }
 
-void setTargetVelocity(int speed) {
-  byte cmd = 0x52; // 'Write data for object of 16 bits' のコマンド
-  int addr = 0x70B1; // 'Target velocity_rpm' のアドレス
-  byte addrH = highByte(addr);
-  byte addrL = lowByte(addr);
-  byte speedH = highByte(speed);
-  byte speedL = lowByte(speed);
-  byte data[] = {0x01, cmd, addrH, addrL, 0x00, 0x00, 0x00, speedL, speedH};
+void sendVelocityCommand(int address, int rpm) {
+  byte addrH = highByte(address);
+  byte addrL = lowByte(address);
+  byte rpmH = highByte(rpm);
+  byte rpmL = lowByte(rpm);
+  byte data[] = {0x01, 0x52, addrH, addrL, 0x00, 0x00, 0x00, rpmL, rpmH};
   byte checksum = calculateChecksum(data, sizeof(data));
 
   mySerial.write(data, sizeof(data)); // コマンドを送信
   mySerial.write(checksum); // チェックサムを送信
-
-  // モータードライバからの応答を受信
-  if (waitForResponse()) {
-    Serial.println("Speed set successfully.");
-  } else {
-    Serial.println("Failed to set speed.");
-  }
 }
 
 byte calculateChecksum(byte *data, int len) {
@@ -41,19 +42,4 @@ byte calculateChecksum(byte *data, int len) {
     sum += data[i];
   }
   return sum & 0xFF; // チェックサム計算
-}
-
-bool waitForResponse() {
-  unsigned long startTime = millis();
-  while (millis() - startTime < 500) { // 500ミリ秒待つ
-    if (mySerial.available() >= 10) { // 応答の長さが10バイトの場合
-      while (mySerial.available()) {
-        Serial.print(mySerial.read(), HEX); // 応答をヘックス形式で表示
-        Serial.print(" ");
-      }
-      Serial.println();
-      return true;
-    }
-  }
-  return false;
 }
