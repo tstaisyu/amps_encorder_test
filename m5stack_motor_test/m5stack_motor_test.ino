22e9a52a2196c8a84b4e033fa3ce227497812555
taisyu shibata <t_shiba117@yahoo.co.jp>
2024-07-07 06:31:42 +0900

    Return to simple motor test

--- m5stack_motor_test/m5stack_motor_test.ino
+++ m5stack_motor_test/m5stack_motor_test.ino
@@ -19,16 +19,14 @@ void setup() {
   // モーターを有効化
   sendCommand(0x7019, 0x52, 0x0F, 0x00);
   delay(100);
+
+  sendCommand(0x70B1, 0x52, 0x04, 0x00);
+  delay(100);
+
+
 }
 
 void loop() {
-  if (Serial.available()) {
-    int velocity_rpm = Serial.parseInt();
-    if (velocity_rpm > 0) {
-      int velocity_dec = calculateVelocityDEC(velocity_rpm);
-      sendVelocityDEC(velocity_dec);
-    }
-  }
 
   // レスポンスを受信して表示
   if (mySerial.available()) {
@@ -43,12 +41,6 @@ void loop() {
     }
     Serial.println();
   }
-}
-
-void sendVelocityDEC(int velocity_dec) {
-  int lower_byte = velocity_dec & 0xFF;
-  int upper_byte = (velocity_dec >> 8) & 0xFF;
-  sendCommand(0x70B2, 0x54, lower_byte, upper_byte); // Target Velocity_DECを設定
 }
 
 void sendCommand(uint16_t address, byte cmd, uint16_t data, byte msbData) {
@@ -81,10 +73,4 @@ byte calculateChecksum(byte *data, int len) {
     sum += data[i];
   }
   return sum & 0xFF; // チェックサム計算
-}
-
-int calculateVelocityDEC(int rpm) {
-  // 速度変換式 [DEC] = ([rpm]*512*4096)/1875;
-  long dec = long(rpm) * 512L * 4096L / 1875L;
-  return int(dec);
 }
