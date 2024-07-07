026d682a28a67a1eaa2a24f33c33715c92116d13
taisyu shibata <t_shiba117@yahoo.co.jp>
2024-07-07 11:06:27 +0900

    Add microROS communication logic

--- M5Stack_AMPS_HubWheel_Control/M5Stack_AMPS_HubWheel_Control.ino
+++ M5Stack_AMPS_HubWheel_Control/M5Stack_AMPS_HubWheel_Control.ino
@@ -13,10 +13,41 @@
  * limitations under the License.
  */
 
+#include "BluetoothSerial.h"
+#include <micro_ros_arduino.h>
 #include <HardwareSerial.h>
+
+#include <M5Stack.h>
+#include <stdio.h>
+#include <rcl/rcl.h>
+#include <rcl/error_handling.h>
+#include <rclc/rclc.h>
+#include <rclc/executor.h>
+
+#include <geometry_msgs/msg/twist.h>
+
+#include "CytronMotorDriver.h"
+
+#include "esp32-hal-timer.h"
+
+BluetoothSerial SerialBT;
 
 // ハードウェアシリアル設定
 HardwareSerial mySerial(2); // RX=16, TX=17を使用
+
+rcl_subscription_t subscriber;
+geometry_msgs__msg__Twist msg;
+rclc_executor_t executor;
+rclc_support_t support;
+rcl_allocator_t allocator;
+rcl_node_t node;
+rcl_timer_t timer;
+
+//rcl_init_options_t init_options; // Humble
+//size_t domain_id = 117;
+
+#define RCCHECK(fn) { rcl_ret_t temp_rc = fn; if ((temp_rc != RCL_RET_OK)) {Serial.println("Error in " #fn); return;}}
+#define RCSOFTCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){}}
 
 // UARTピン設定
 const int RX_PIN = 16; // RXピン
@@ -44,26 +75,37 @@ const int BAUD_RATE = 115200;
 const byte MOTOR_ID = 0x01;
 const byte ERROR_BYTE = 0x00; // エラーバイトは必要に応じて調整
 
-void setup() {
-  Serial.begin(BAUD_RATE);
-  mySerial.begin(BAUD_RATE, SERIAL_8N1, RX_PIN, TX_PIN);
+double speed_ang = 0.0;
+double speed_lin = 0.0;
 
-  Serial.println("Starting motor setup...");
+void subscription_callback(const void * msgin) {
+  M5.Speaker.begin();
+  M5.Lcd.setCursor(0, 20);  
+  M5.Lcd.println("Callback triggered");
+  const geometry_msgs__msg__Twist * msg = (const geometry_msgs__msg__Twist *)msgin;
 
-  // オペレーションモードを速度制御モードに設定
-  sendCommand(OPERATION_MODE_ADDRESS, WRITE_COMMAND, OPERATION_MODE_SPEED_CONTROL);
-  delay(100);
+  // 受信したメッセージの内容をシリアルポート経由で出力
+  Serial.print("Received linear.x: ");
+  Serial.println(msg->linear.x);
+  Serial.print("Received angular.z: ");
+  Serial.println(msg->angular.z);
 
-  // エマージェンシーストップを解除
-  sendCommand(EMERGENCY_STOP_ADDRESS, WRITE_COMMAND, DISABLE_EMERGENCY_STOP);
-  delay(100);
+  M5.Lcd.clear();  // LCD画面をクリア
+  M5.Lcd.setCursor(0, 20);  // テキスト表示位置を設定
+  M5.Lcd.print("Callback triggered");
 
-  // モーターを有効化
-  sendCommand(CONTROL_WORD_ADDRESS, WRITE_COMMAND, ENABLE_MOTOR);
-  delay(100);
-}
+  M5.Lcd.setCursor(0, 40);
+  M5.Lcd.print("Linear.x: ");
+  M5.Lcd.println(msg->linear.x);
 
-void loop() {
+  M5.Lcd.setCursor(0, 60);
+  M5.Lcd.print("Angular.z: ");
+  M5.Lcd.println(msg->angular.z);
+
+  // 速度と角速度を取得
+  speed_lin = msg->linear.x;
+  speed_ang = msg->angular.z;
+
   static unsigned long lastSendTime = 0;
   const int sendInterval = 100; // ミリ秒単位でコマンド送信間隔
   static int last_velocity_dec = 0; // 最後に送信した速度を保持する変数
@@ -102,6 +144,73 @@ void loop() {
       Serial.print(" ");
     }
     Serial.println();
+  }
+
+}
+
+void setup() {
+  Serial.begin(BAUD_RATE);
+  while(!Serial);  // シリアルポートが開くのを待つ
+  mySerial.begin(BAUD_RATE, SERIAL_8N1, RX_PIN, TX_PIN);
+
+  Serial.println("Starting motor setup...");
+
+  // オペレーションモードを速度制御モードに設定
+  sendCommand(OPERATION_MODE_ADDRESS, WRITE_COMMAND, OPERATION_MODE_SPEED_CONTROL);
+  delay(100);
+
+  // エマージェンシーストップを解除
+  sendCommand(EMERGENCY_STOP_ADDRESS, WRITE_COMMAND, DISABLE_EMERGENCY_STOP);
+  delay(100);
+
+  // モーターを有効化
+  sendCommand(CONTROL_WORD_ADDRESS, WRITE_COMMAND, ENABLE_MOTOR);
+  delay(100);
+
+  M5.begin();
+  delay(500);
+  M5.Lcd.setTextSize(2);
+  M5.Lcd.setCursor(0, 0);  // ステータスメッセージの位置を設定
+  M5.Lcd.print("micro ROS2 M5Stack START\n");  
+
+	// USB経由の場合
+	set_microros_transports();
+
+//  delay(2000);
+
+  allocator = rcl_get_default_allocator();
+
+  RCCHECK(rclc_support_init(&support, 0, NULL, &allocator));
+
+	//init_options = rcl_get_zero_initialized_init_options();
+	//RCCHECK(rcl_init_options_init(&init_options, allocator));
+	//RCCHECK(rcl_init_options_set_domain_id(&init_options, domain_id));		// ドメインIDの設定
+	//RCCHECK(rclc_support_init_with_options(&support, 0, NULL, &init_options, &allocator)); // 前のrclc_support_initは削除する
+  RCCHECK(rclc_node_init_default(&node, "subscriber_node", "", &support));
+
+  RCCHECK(rclc_subscription_init_best_effort(
+    &subscriber,
+    &node,
+    ROSIDL_GET_MSG_TYPE_SUPPORT(geometry_msgs, msg, Twist),
+    "/cmd_vel"));
+
+	int callback_size = 1;	// コールバックを行う数
+//	executor = rclc_executor_get_zero_initialized_executor();
+  RCCHECK(rclc_executor_init(&executor, &support.context, callback_size, &allocator));
+  RCCHECK(rclc_executor_add_subscription(&executor, &subscriber, &msg, &subscription_callback, ON_NEW_DATA));
+}
+
+void loop() {
+  RCSOFTCHECK(rclc_executor_spin_some(&executor, RCL_MS_TO_NS(100)));
+  if (!initial_data_received && (millis() - last_receive_time > RECEIVE_TIMEOUT)) {
+    Serial.printf("No data received for %d seconds, restarting...\n", RECEIVE_TIMEOUT / 1000);
+    ESP.restart();
+  }
+  delay(100);
+  if (rcl_error_is_set()) {
+    RCL_SET_ERROR_MSG("rclc_executor_spin_some failed");
+    printf("Error in rclc_executor_spin_some: %s\n", rcl_get_error_string().str);
+    rcl_reset_error();
   }
 }
 
