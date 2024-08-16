77ea1ee62cbeb433b1a9fb2dc37bae27a60dd571
taisyu shibata <t_shiba117@yahoo.co.jp>
2024-08-16 23:44:19 +0900

    Refactor: Move functions to RosCommunications.cpp

--- src/RosCommunication.cpp
+++ src/RosCommunication.cpp
@@ -13,3 +13,32 @@
  * limitations under the License.
  */
 
+void logReceivedData(const geometry_msgs__msg__Twist *msg) {
+  Serial.print("Received linear.x: ");
+  Serial.println(msg->linear.x);
+  Serial.print("Received angular.z: ");
+  Serial.println(msg->angular.z);
+}
+
+void updateDisplay(const geometry_msgs__msg__Twist *msg) {
+  M5.Lcd.clear();  // LCD画面をクリア
+  M5.Lcd.setCursor(0, 20);  // テキスト表示位置を設定
+  M5.Lcd.print("Callback triggered");
+
+  M5.Lcd.setCursor(0, 40);
+  M5.Lcd.print("Linear.x: ");
+  M5.Lcd.println(msg->linear.x);
+
+  M5.Lcd.setCursor(0, 60);
+  M5.Lcd.print("Angular.z: ");
+  M5.Lcd.println(msg->angular.z);
+}
+
+void initMotor(HardwareSerial& serial, byte motorID) {
+    motorController.sendCommand(motorID, OPERATION_MODE_ADDRESS, MOTOR_SETUP_COMMAND, OPERATION_MODE_SPEED_CONTROL);
+    delay(COMMAND_DELAY);
+    motorController.sendCommand(motorID, EMERGENCY_STOP_ADDRESS, MOTOR_SETUP_COMMAND, DISABLE_EMERGENCY_STOP);
+    delay(COMMAND_DELAY);
+    motorController.sendCommand(motorID, CONTROL_WORD_ADDRESS, MOTOR_ENABLE_COMMAND, ENABLE_MOTOR);
+    delay(COMMAND_DELAY);
+}
